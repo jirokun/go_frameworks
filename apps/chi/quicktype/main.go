4e597a9496d940017489569bdf3dcfa577a5556d
Jiro Iwamoto <jirokun@gmail.com>
2022-02-03 16:27:14 +0900

    不要なコード削除

--- apps/chi/quicktype/main.go
+++ apps/chi/quicktype/main.go
@@ -2,19 +2,8 @@ package main
 
 import (
 	"net/http"
-
-	"github.com/go-chi/docgen"
 )
 
 func main() {
 	http.ListenAndServe(":3000", routes())
-}
-
-func newMarkdownOpts() docgen.MarkdownOpts {
-	return docgen.MarkdownOpts{
-		ProjectPath:        "",
-		Intro:              "",
-		ForceRelativeLinks: false,
-		URLMap:             map[string]string{},
-	}
 }
